ae208ff9c98350e7e0743d0c4343a3f33d440c12
enthomas <enthomas@ensc.fr>
2020-04-14 02:42:14 +0200

    contenu  questions

--- db/content.sql
+++ db/content.sql
@@ -1,8 +1,8 @@
 INSERT INTO THEME VALUES 
 (1, 'Star Wars', 24, 'star-wars.png'),
 (2, 'Harry Potter', 29, 'harry-potter.png'),
-(3, 'Cuisine', 0, 'cuisine.png'),
-(4, 'Géographie', 0, 'geographie.png');
+(3, 'Cuisine', 22, 'cuisine.png'),
+(4, 'Géographie', 26, 'geographie.png');
 
 
 INSERT INTO QUESTION VALUES 
@@ -61,6 +61,55 @@ INSERT INTO QUESTION VALUES
     (27, 2, 'qcm', "Qui sont les langues de plombs ?", 'Des sorciers qui travaillent au département des mystères', 'Des sorciers qui travaillent le plomb', 'Des sorciers qui travaillent au département des créatures magiques', 'Des objets ensorcelés'),
     (28, 2, 'qcm', "Que fait l'épouvantard ?", 'Il prend la forme de ce qui nous fait peur', 'Il nous tue', 'Il aspire notre bonheur', 'Il ne fait rien car on ne le voit pas'),
     (29, 2, 'qcm', "Que signifie ASPIC ?", 'Accumulation de Sorcellerie Particulièrement Intensive et Contraignante', 'Aspiration de Sorcellerie Prematurement Inoffensive et Contraignante', 'Assortiment de Sorciers  Pour un Intensif Concours', 'Ca ne veur rien dire')    ;
+    #cuisine
+    (1, 3, 'vrai_faux', "Le ttoro, soupe de poissons, est une spécialité bretonne.", 'Faux', '', '', ''),
+    (2, 3, 'vrai_faux', "Le Paris-Brest est une pâtisserie composée d'une pâte à choux fourrée d'une crème au café.", 'Faux', '', '', ''),
+    (3, 3, 'vrai_faux', "Les trois ingrédients qui suffisent à préparer l'authentique truffade, sont les pommes de terre, le fromage et l'ail (sans oublier sel et poivre, bien sûr).", 'Vrai', '', '', ''),
+    (4, 3, 'vrai_faux', "La meurette, sauce au vin rouge cuisinée avec des lardons, des oignons, des champignons, est typiquement bordelaise.", 'Faux', '', '', ''),
+    (5, 3, 'vrai_faux', "On appelle 'cervelle de canut', du fromage blanc en faisselle assaisonné avec ciboulette, persil et échalote hachés, huile d'olive, sel et poivre.", 'Vrai', '', '', ''),
+    (6, 3, 'vrai_faux', "La garbure béarnaise ne contient pas de confit de canard.", 'Vrai', '', '', ''),
+    (7, 3, 'vrai_faux', "La sauce Nantua est élaborée à partir d'une béchamel et de beurre de langoustine.", 'Faux', '', '', ''),
+    (8, 3, 'vrai_faux', "La bourride à la sétoise est un ragoût composé de poissons blancs dont la lotte (queue de baudroie) et accompagné d'aïoli.", 'Vrai', '', '', ''),
+    (9, 3, 'vrai_faux', "Vacherin ! Ce nom désigne à la fois plusieurs sortes de fromages et un gâteau.", 'Vrai', '', '', ''),
+    (10, 3, 'vrai_faux', "On appelle 'koulak' un pâté en croûte d'origine russe, farci de poisson ou de viande, d'oeufs durs et de légumes.", 'Faux', '', '', ''),
+    (11, 3, 'vrai_faux', "Dans le commerce, les oeufs marqués '0' sont les oeufs de poules élévées en plein air et nourries avec une alimentation biologique.", 'Vrai', '', '', ''),
+    (12, 3, 'qcm', "De quel pays viennent les moules frites ?", 'Belgique', 'Espagne', 'Hollande', 'Italie'),
+    (13, 3, 'qcm', "On vous sert un poulet Tandoori. Dans quel restaurant êtes-vous ?", 'Restaurant indien', 'Restaurant japonais', 'Au Tex Mex', 'Restaurant chinois'),
+    (14, 3, 'qcm', "On nous présente une soupe nommée 'harira', où sommes-nous ?", 'Au Maroc', 'En Tunisie', 'Au Liban', 'En Arabie'),
+    (15, 3, 'qcm', "Dans quel restaurant trouvons-nous des sushis ?", 'Japonais', 'Chinois', 'Grec', 'Thaïlandais'),
+    (16, 3, 'qcm', "D'où viennent les beignets farcis à la morue ?", 'Aux Antilles', 'Au Brésil', 'Aux Pjilippines', 'Au Portugal'),
+    (17, 3, 'qcm', "On nous les sert en Italie à l'apéro :", 'Les antipasti', 'Les tapas', 'Las zakouski', 'Les acras'),
+    (18, 3, 'qcm', "Où nous proposera-t-on des fajitas ?", 'Au Tex Mex', 'Au restaurant indien', 'Au restaurant libanais', 'Au restaurent marocain'),
+    (19, 3, 'qcm', "Dans quel restaurant sert-on de la soupe 'Pho' ?", 'Vietnamien', 'Chinois', 'Cambodjien', 'Thaïlandais'),
+    (20, 3, 'qcm', "Spécialité anglaise, qu'est-ce que le black pudding ?", 'Du boudin noir', 'Un pâté aux rogons de mouton', 'Un fondant au chocolat', 'Un plat à base de Marmite'),
+    (21 3, 'qcm', "Quel est le nom du ragoût de veau basque ?", 'Axoa', 'Ttoro', 'Tripota', 'Muxu'),
+    (22, 3, 'qcm', "Le bourou bourou est une soupe de légumes et de pâtes, de quel pays est-elle la spécialité ?", 'Grèce', 'Gabon', 'Liban', 'Inde'),
+    #Géographie
+    (1, 4, 'qcm', "Laquelle de ces villes n'est pas une capitale ?", 'Amalty', 'Skopje', 'Tirana', 'Noursoultan'),
+    (2, 4, 'qcm', "Quelle langue parle-t-on au Chili ?", 'Espagnol', 'Portigais', 'Italien', 'Français'),
+    (3, 4, 'qcm', "De quel pays l'ouguiya est-elle la monnaie ?", 'Mauritanie', 'Burkina Faso', 'Ghana', 'Gabon'),
+    (5, 4, 'qcm', "Sur quel continent se situe le Sri Lanka ?", 'Asie', 'Océanie', 'Afrique', 'Europe'),
+    (6, 4, 'qcm', "Quelle est la capitale constitutionnelle de la Bolivie ?", 'Sucre', 'La Paz', 'Bogota', 'Santa Cruz de la Sierra'),
+    (7, 4, 'qcm', "De quel pays le Pérou est-il devenu indépendant le 28 Juillet 1821 ?", 'Espagne', 'Italie', 'Portugal', 'France'),
+    (8, 4, 'qcm', "Où se situe Fortaleza ?", 'Brésil', 'Argentine', 'Uruguay', 'Chili'),
+    (9, 4, 'qcm', "Ses habitants sont appelés les Maralpins. Quel est ce département français ?", 'Les Alpes-Maritimes', 'Les Hautes-Alpes', 'Les Alpes-de-Haute-Provence', 'La Charente-Maritime'),
+    (10, 4, 'qcm', "Quelle est la préfecture du Calvados ?", 'Caen', 'Rouen', 'Amien', 'Missy'),
+    (11, 4, 'qcm', "Quel est code Insee des Pyrénées-Orientales ?", '66', '64', '65', '67'),
+    (12, 4, 'vrai_faux', "La frontière franco-italienne sépare l'Italie et cinq départements français dont l'Isère.", 'Faux', '', '', ''),
+    (13, 4, 'qcm', "A quelle région la presqu'île de Quiberon est-elle rattachée ?", 'Bretagne', 'Normandie', 'Pays de la Loire', 'Île de France'),
+    (14, 4, 'vrai_faux', "Ajaccio est la préfecture du département de la Haut-Corse.", 'Faux', '', '', ''),
+    (15, 4, 'qcm', "Quel est le code Insee du Haut-Rhin ?", '68', '66', '67', '69'),
+    (16, 4, 'qcm', "Quel est lecode Insee de La Réunion ?", '974', '972', '973', '975'),
+    (17, 4, 'qcm', "Quelle est la préfecture de la Moselle (57) ?", 'Metz', 'Strasbourg', 'Nancy', 'Toul'),
+    (18, 4, 'qcm', "Elle fut la capitale des Indes néerlandaises de 1799 à 1942 sous le nom de Batavia. C'est maintenant la capitale du plus grand archipel du monde, composé de plus de 17000 îles. C'est :", 'Jakarta', 'Tokyo', 'Manille', 'Hong-Kong'),
+    (19, 4, 'qcm', "Quel est la couleur du rond se trouvant au centre du drapeau du Japon ?", 'Rouge', 'Jaune', 'Violet', 'Blanc'),
+    (20, 4, 'qcm', "Comment appelle-t-on les habitants de la ville de Reims ?", 'Rémois', 'Rémais', 'Rémons', 'Reimsés'),
+    (21, 4, 'qcm', "En quelle année l'Inde est-elle devenue indépendante ?", '1947', '1927', '1937', '1957'),
+    (22, 4, 'qcm', "De combiende départements est composée la région Nouvelle-Aquitaine ?", '12', '8', '10', '16'),
+    (23, 4, 'qcm', "Où se situe le pont Banpo ?", 'Corée de Sud', 'Thaïlande', 'Cambodge', 'Japon'),
+    (24, 4, 'qcm', "En quelle année l'Autriche a adhéré à l'Union Européenne ?", '1995', '1984', '1990', '1999'),
+    (25, 4, 'qcm', "Dans quelle province canadienne se trouve la ville de Toronto ?", 'Ontario', 'Manitoba', 'Québec', 'Albreta'),
+    (26, 4, 'qcm', "Sur quel continent se trouve la Somalie ?", 'Afrique', 'Amérique', 'Océanie', 'Asie'),
 ;
 INSERT INTO UTILISATEUR VALUES 
     (1, 'moi', 'motdepasse', 'user');
