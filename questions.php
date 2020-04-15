43d2b6ec213e4b4ed687dfa525143930e01bcde4
Elisa003 <eguerin003@ensc.fr>
2020-04-15 13:31:36 +0200

    compréhension des pbs, mais pas résolution...

--- questions.php
+++ questions.php
@@ -8,9 +8,14 @@ $diffId = $_GET['id2'];
 $bdd = getDb();
 
 //récupération du nombre de questions associées au thème
-$requete = $bdd->prepare("select nb_questions from THEME where id_theme= ? ");
-$requete -> execute(array($themeId));
-$nbQuestTheme = $requete->fetch();
+$requete = $bdd->prepare('SELECT nb_questions FROM THEME WHERE id_theme=?');
+$requete->execute(array($themeId));
+$nbQuestTheme = intval($requete->fetch());
+
+//PROBLEME : nbQuestTheme = 1 --> c'est pour ça qu'il nous affiche qu'une seule question
+echo "nbQuestTheme = ". $nbQuestTheme;
+
+
 //récupération du nombre de questions associées à la difficulté
 $requete = $bdd->prepare("select nb_questions from DIFFICULTE where id_difficulte=?");
 $requete->execute(array($diffId));
@@ -18,6 +23,7 @@ $nbQuest = intval($requete->fetch());
 //création d'un tableau contenant l'id des questions sélectionnées pour le questionnaire
 $tabInd = range(1, $nbQuestTheme, 1); // tableau [|1;2;3;...;$nbQuestTheme|]
 $questSelect = array(array_rand($tabInd, $nbQuest)); // sélectionne $nbQuest éléments dans $tabInd et les met dans $questSelect
+
 ?>
 
 <?php if (isUserConnected()) 
@@ -25,17 +31,18 @@ $questSelect = array(array_rand($tabInd, $nbQuest)); // sélectionne $nbQuest é
 ?>
     <form method="POST" action="resultat.php?id1=<?=$themeId?>&id2=<?=$diffId?>">
         <?php
-        foreach ($questSelect as $idQuest) //c'était foreach($questSelect as $idQuest) je sais pas si ça change qqchose dans le contenu des questions du coup
-        //Ah alors je crois que ça affiche toutes les questions au lieu des questions par niveaux
-        //et ça les affiche une par une ... je crois que c'était pas ce qui était prévu ^^
-        // c'est bien $questSelect car sinon c'est effectivement toutes les questions
+        foreach ($questSelect as $idQuest) 
         {
-            $question = getQuestion($idQuest, $themeId, $bdd);
-            echo $question['question'] ;
+            $question = getQuestion($idQuest+1, $themeId, $bdd);
+            ?>
+            <div class="form-group">
+                <label><?= $question['question'] ?></label>
+            <?php
+            
             if ($question['type'] == "qcm") 
             {
                 ?></br>
-                <select name="reponse<?= $question['id_question'] ?>[]" size="4">
+                <select class="form-control" name="reponse[<?= $question['id_question'] ?>]" size="4">
                     <option value="reponsefausse1"><?= $question['reponse_fausse1'] ?></option>
                     <option value="reponsefausse2"><?= $question['reponse_fausse2'] ?></option>
                     <option value="reponsevraie"><?= $question['reponse_vraie'] ?></option>
@@ -47,20 +54,22 @@ $questSelect = array(array_rand($tabInd, $nbQuest)); // sélectionne $nbQuest é
             {
                 ?></br>
                 <label for="vrai">Vrai</label>
-                <input type="radio" name="reponse<?= $question['id_question'] ?>"/>
+                <input type="radio" name="reponse[<?= $question['id_question']?>]"/>
                 <label for="faux">Faux</label>
-                <input type="radio" name="reponse<?= $question['id_question'] ?>"/>
+                <input type="radio" name="reponse[<?= $question['id_question']?>]"/>
                 <?php
             }
             else 
             {
                 ?></br>
-                <input type="text" name="reponse<?= $question['id_question'] ?>" size="50" /><br>
+                <input type="text" name="reponse[<?= $question['id_question'] ?>]" size="50" /><br>
                 <?php
-            }
-            $_SESSION["id_question"] = $idQuest;
+            }?>
+            </div>
+            <?php
         }
         ?>
+        
         <input type="submit" value="Envoyer">
     </form>
 <?php
--- resultat.php
+++ resultat.php
@@ -1,14 +1,15 @@
 <?php
+require_once "includes/header.php" ;
 require_once "includes/functions.php";
 $themeId = $_GET['id1'];
 $diffId = $_GET['id2'];
 $bdd = getDb();
-$idQuest = $_SESSION["id_question"]; //PROBLEME : Variable de session pas reconnue :'(
+//$idQuest = $_SESSION["id_question"]; //PROBLEME : Variable de session pas reconnue :'(
   //dans l'idée il faudrait qu'on récupère toutes les réponses d'un coup ici, du coup on aurait pas besoin de cette variable de session 
   //car il y aurait plein de questions différentes
-if(isset($_POST["question"]))
+if(isset($_POST["reponse[]"]))
 {
-    $reponse_utilisateur = $_POST["question"];
+    $reponse_utilisateur = escape($_POST["reponse[]"]);
     $requete = $bdd->prepare("select reponse_vraie from QUESTION where id_theme= ? and id_question= ?");
     $requete -> execute(array($themeId, $idQuest));
     $reponse_vraie = $requete->fetch();
